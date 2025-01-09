139fbea4423516257bc8fc394c2bad890a2aecf9
WLawson <William.Lawson@kmcsystems.com>
2025-01-09 14:16:51 -0500

    fiddling

--- Net8Assembly/DoSomething.cs
+++ Net8Assembly/DoSomething.cs
@@ -2,6 +2,6 @@
 {
     public static class DoSomething
     {
-        public static string Do() => ".NET 8 assembly";
+        public static string Do() => "static method in .NET 8 assembly";
     }
 }
--- README.md
+++ README.md
@@ -3,8 +3,4 @@
 Todo:
 
 	1. build a basic IronPython implementation
-	2. build a multi-threaded implementation(s)
-		- Python-based / CLR-based
-			- CPU-bound
-			- I/O-bound
-	3.
+	
